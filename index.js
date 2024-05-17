9ff5638361bbd0e8cdc66cc25dccd8745186074f
Julian <birdman.coding.1991@gmail.com>
2024-05-17 16:57:02 +0200

    fs

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-import {add} from './lib/utilities.js'
+// import _ from 'lodash'
 
-const objOne = {
-    name: "Tom",
-    age: 35
-}
 
-const objTwo = objOne
+// function add(a,b) {
+//     return a+b
+// }
 
-console.log(objOne === objTwo);
+// add()
+
+// const objOne = {
+//     name: "Tom",
+//     age: 35
+// }
+
+// const objTwo = {
+//     name: "Tom",
+//     age: 35
+// }
+
+// console.log(_.isEqual(objOne, objTwo));
+
+import fs from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const directory = fs.readdirSync(__dirname)
+const filePath = __dirname + "/test.txt"
+const testFile = fs.readFileSync(filePath, {encoding: "utf-8"})
+console.log(testFile);
+
+fs.writeFileSync(filePath, "hello")
+
